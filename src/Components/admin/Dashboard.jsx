134c4e9ac301ab71ba8500ae8849ada092996ffd
Ale <alearanibar@gmail.com>
2020-11-11 22:57:07 -0800

    update

---
+++ src/Components/admin/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AdminLayout from '../../HOC/AdminLayout'
+
+
+const Dashboard = () => {
+    return (
+      <AdminLayout>
+        <div className="user_dashboard"><div>dash</div></div>
+      </AdminLayout>
+    );
+}
+
+export default Dashboard
